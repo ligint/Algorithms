d810905506f6f3e91bef417d5f508a115cfa1a8f
Ligin Thiya <ligin.thiya@gmail.com>
2020-04-01 05:48:38 -0700

    LeetCode Problem

---
+++ LeetCode/TaskScheduler.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] map = new int[26];
+        for(char c : tasks)
+            map[c - 'A']++;
+        
+        Arrays.sort(map);
+        
+        int maxVal = map[25] - 1 , idleSlots = maxVal * n;
+        for(int i = 24; i >= 0 && map[i] > 0; i--){
+            
+            idleSlots -= Math.min(map[i], maxVal);
+        }
+        
+        return idleSlots > 0 ? idleSlots + tasks.length : tasks.length;
+    }
+}
