063c13a61f2c6445e24b593d45b0e375b238786d
Ligin Thiya <ligin.thiya@gmail.com>
2020-04-06 00:00:22 -0700

    LeetCode problems Day 6

---
+++ LeetCode/ValidParenthesis.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        if(s == null || s.length() == 0)
+            return true;
+        Stack<Character> stack = new Stack();
+        
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '(' || c =='{' || c == '['){
+                
+                stack.push(c);
+            }
+            else{
+                
+                if(c == ')'){
+                    if(stack.isEmpty() || stack.pop() != '(')
+                        return false;
+                }
+                
+                if(c == '}'){
+                    if(stack.isEmpty() || stack.pop() != '{')
+                        return false;
+                }
+                
+                if(c == ']'){
+                    if(stack.isEmpty() || stack.pop() != '[')
+                        return false;
+                }
+                
+            }
+            
+        }
+        
+        return stack.isEmpty();
+        
+    }
+}
