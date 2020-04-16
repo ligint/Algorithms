c7c0104c1f70f85865096d5246bd187f3ca2ff36
Ligin Thiya <ligin.thiya@gmail.com>
2020-04-16 15:31:05 -0700

    Solution for Decode String

---
+++ LeetCode/DecodeString.java
@@ -0,0 +1,48 @@
+class Solution {
+    public String decodeString(String s) {
+        
+        Stack<Integer> counts = new Stack();
+        Stack<String> result = new Stack();
+        
+        String res ="";
+        
+        int index = 0;
+        
+        while(index < s.length()){
+        
+            
+            if(Character.isDigit(s.charAt(index))) {
+              
+                int count = 0;
+                while(Character.isDigit(s.charAt(index))) {
+                    
+                    count = 10 * count + (s.charAt(index) - '0');
+                    index += 1;
+                }
+                counts.push(count);
+            } else if(s.charAt(index) == '['){
+                
+                result.push(res);
+                res = "";
+                index += 1;
+            } else if(s.charAt(index) == ']'){
+                
+                StringBuilder sb = new StringBuilder(result.pop());
+                int count = counts.pop();
+                for(int i = 0; i < count ; i++){
+                    
+                    sb.append(res);
+                }
+                res = sb.toString();
+                index += 1;
+            }
+            else{
+                
+                res += s.charAt(index);
+                index += 1;
+            }
+            
+        }
+        return res;
+    }
+}
