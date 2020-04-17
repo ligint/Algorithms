e1890bbc2424a5e5c0a73a4c95385998330b34c0
Ligin Thiya <ligin.thiya@gmail.com>
2020-04-16 17:11:35 -0700

    Solution for Friend Circles

---
+++ LeetCode/FriendCircles.java
@@ -0,0 +1,29 @@
+class Solution {
+    
+    public void dfs(int[][] M, boolean[] visited, int i){
+        
+        for(int j = 0; j < M.length; j++){
+            
+            if(M[i][j] == 1 && !visited[j]){
+                
+                visited[j] = true;
+                dfs(M,visited,i);
+            }
+        }
+    }
+    public int findCircleNum(int[][] M) {
+     
+        boolean[] visited = new boolean[M.length];
+        int count = 0;
+        
+        for(int i = 0; i < M.length ; i++){
+            
+            if(!visited[i]){
+                
+                dfs(M,visited,i);;
+                count++;
+            }
+        }
+        return count;
+    }
+}
