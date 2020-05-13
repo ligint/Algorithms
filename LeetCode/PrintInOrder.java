896233f75b477fd963d315ea68c6386dd476262d
Ligin Thiya <ligin.thiya@gmail.com>
2020-05-13 15:20:58 -0700

    Solution of Print In Order

---
+++ LeetCode/PrintInOrder.java
@@ -0,0 +1,32 @@
+class Foo {
+
+    Semaphore first, second;
+    
+    public Foo() {
+        first = new Semaphore(0);
+        second = new Semaphore(0);
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        first.release();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        first.acquire();
+        printSecond.run();
+        second.release();
+        
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        
+        // printThird.run() outputs "third". Do not change or remove this line.
+        second.acquire();
+        printThird.run();
+    }
+}
