ed1648ff5117a0de356b7da95ce1822e6f1a9a46
Ligin Thiya <ligin.thiya@gmail.com>
2020-05-16 14:18:33 -0700

    Solution for Rotate List

---
+++ LeetCode/RotateList.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || head.next == null)
+            return head;
+        
+        int size = 1;
+        ListNode fast = head;
+        ListNode slow = head;
+        
+        while(fast.next != null){
+            size++;
+            fast = fast.next;
+        }
+        
+        for(int i = size -(k % size); i > 1; i--)
+            slow = slow.next;
+        
+        fast.next = head;
+        head = slow.next;
+        slow.next = null;
+        
+        return head;
+    }
+}
