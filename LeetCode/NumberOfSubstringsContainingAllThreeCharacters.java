f466ce2cf0fee13fa798bb6e1e25a14b7aed71b7
Ligin Thiya <ligin.thiya@gmail.com>
2020-06-09 13:20:34 -0700

    Solution for Number of Substrings Containing All Three Characters

---
+++ LeetCode/NumberOfSubstringsContainingAllThreeCharacters.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numberOfSubstrings(String s) {
+        
+        int[] count = {0 , 0 ,0};
+        int result = 0, i = 0, n = s.length();
+        
+        for(int j = 0 ; j < n ; j++){
+            
+            count[s.charAt(j) - 'a']++;
+            while(count[0] > 0 && count[1] > 0 && count[2] > 0){
+                count[s.charAt(i++) - 'a']--;
+            }
+            result += i;
+        }
+        return result;
+    }
+}
