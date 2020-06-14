56fd2237d2d89d75ea718287006563efc9cdfe69
Ligin Thiya <ligin.thiya@gmail.com>
2020-06-14 01:36:11 -0700

    Solution for Palindrome Permutation

---
+++ LeetCode/PalindromePermutation.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canPermutePalindrome(String s) {
+        HashSet<Character> set = new HashSet<>();
+        for(int i = 0; i < s.length(); i++){
+            if(!set.add(s.charAt(i)))
+                set.remove(s.charAt(i));
+        }
+        
+        return set.size() <= 1;
+    }
+}
